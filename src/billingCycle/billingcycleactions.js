82c75206f2876ec045dab020e2356f758a243a9e
ernetwork <wanderwang@hotmail.com>
2018-07-27 09:03:27 -0300

    melhorias na inclusão do Ciclo de Pagamento

--- src/billingCycle/billingcycleactions.js
+++ src/billingCycle/billingcycleactions.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { selectTab, showTabs } from '../common/tab/tabactions'
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -10,8 +14,19 @@ export function getList() {
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
-    return {
-        type: 'TEMP'
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação Realizada com sucesso!')
+                dispatch([
+                    resetForm('billingcycleform'),
+                    getList(),
+                    selectTab('tablist'),
+                    showTabs('tablist', 'tabcreate')
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error));
+            })
     }
 }
--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import DashboardReducers from '../dashboard/dashboardreducers'
 import TabReducer from '../common/tab/tabreducer'
@@ -9,7 +10,8 @@ const rootReducer = combineReducers({
     dashboard: DashboardReducers,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
-    form: formReducer
+    form: formReducer,
+    toastr: toastrReducer
 
 })
 
